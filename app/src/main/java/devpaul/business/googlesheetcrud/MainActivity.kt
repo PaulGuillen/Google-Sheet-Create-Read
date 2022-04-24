dc74782a4e1ece9024d6e71839b2c4ac78a88695
paulguillen190 <paulguillen190@gmail.com>
2022-04-23 22:09:47 -0500

    Base project

---
+++ app/src/main/java/devpaul/business/googlesheetcrud/MainActivity.kt
@@ -0,0 +1,11 @@
+package devpaul.business.googlesheetcrud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
