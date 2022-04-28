ab445e501bde5f684c7c4b6efee28a20d5e3b1a2
paulguillen190 <paulguillen190@gmail.com>
2022-04-28 12:19:56 -0500

    Updating

--- app/src/main/java/devpaul/business/googlesheetcrud/ReadActivity.kt
+++ app/src/main/java/devpaul/business/googlesheetcrud/ReadActivity.kt
@@ -1,5 +1,6 @@
 package devpaul.business.googlesheetcrud
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,6 +25,7 @@ class ReadActivity : AppCompatActivity() {
     lateinit var  mService : RetrofitService
     lateinit var  adapter : MyDataAdapter
     var recyclerViewData: RecyclerView? = null
+    var progressDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,24 +35,29 @@ class ReadActivity : AppCompatActivity() {
         recyclerViewData = findViewById(R.id.recyclerView)
         recyclerViewData?.layoutManager = LinearLayoutManager(this)
         recyclerViewData?.setHasFixedSize(true)
-
+        progressDialog = ProgressDialog(this)
         getDataList()
     }
 
     private fun getDataList() {
+        progressDialog!!.show()
+        progressDialog!!.setContentView(R.layout.progress_dialog)
+        Objects.requireNonNull(progressDialog!!.window)?.setBackgroundDrawableResource(android.R.color.transparent)
 
         mService.getDataList().enqueue(object : Callback<Data?> {
             override fun onResponse(call: Call<Data?>, response: Response<Data?>) {
                 val jsonResponse: Data? = response.body()
-
           /*      adapter = MyDataAdapter(this@ReadActivity, response.body() as MutableList<Items?>)*/
                 val data2 = jsonResponse?.items as MutableList<Items>
               /*  val data = ArrayList(listOf(jsonResponse?.items))*/
                 adapter = MyDataAdapter(this@ReadActivity, data2)
                 recyclerViewData?.adapter = adapter
+
+                progressDialog?.dismiss()
             }
 
             override fun onFailure(call: Call<Data?>, t: Throwable) {
+                progressDialog?.dismiss()
                 Log.d(TAG,t.message!!)
             }
         })
